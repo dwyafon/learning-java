4ee3f865bcd5c03e310a38b542ddb99759ef67b5
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-02-10 18:13:04 +0000

    add comment notes

---
+++ Comments.java
@@ -0,0 +1,13 @@
+public class Comments {
+    // single-line comment
+    
+    /* 
+    multi-line 
+    comment
+    */
+
+    /**
+     * Javadoc comments -- used when creating docs for APIs; usually written before
+     * declaring classes, methods or fields
+     */
+}
