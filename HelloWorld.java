350a7265164ed0454d295fbc84f42b3715bbe071
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-02-10 17:52:55 +0000

    update comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+public class HelloWorld { // a class is a single concept; same name for file
+    public static void main(String[] args) { // an application must contain a main() method -- app entry point
+        System.out.println("Hej World");
     }
 }
